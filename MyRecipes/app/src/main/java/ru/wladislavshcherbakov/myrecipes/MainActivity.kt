d3f5036b429daf80943fac91f0697152760179cd
Wl4dick <wldmrrcklss@gmail.com>
2025-11-14 16:49:31 +0700

    edit comments

--- MyRecipes/app/src/main/java/ru/wladislavshcherbakov/myrecipes/MainActivity.kt
+++ MyRecipes/app/src/main/java/ru/wladislavshcherbakov/myrecipes/MainActivity.kt
@@ -4,10 +4,7 @@ import android.os.Bundle
 import android.view.View
 import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
@@ -17,13 +14,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Предотвращаем сдвиг контента при открытии клавиатуры
         window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN)
 
-        // Устанавливаем Toolbar как ActionBar
         setSupportActionBar(findViewById(R.id.toolbar))
 
-        // Устанавливаем заголовок
         supportActionBar?.title = getString(R.string.app_name)
 
         setupNavigation()
